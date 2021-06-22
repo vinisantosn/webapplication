5cd95084bd9f97dcf73e9c6df173a0cfdb4c326d
Vinícius Santos <vinisantoosk@gmail.com>
2021-06-22 20:44:13 -0300

    Adicionando script em js no html

---
+++ main.js
@@ -0,0 +1,28 @@
+/*Abre e fecha menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+/*links do menu, quando clicados fecham o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/*adicionar sombra ao header quando o scroll for acionado*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
